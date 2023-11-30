0d6d7f884de81d80268f69baf1adcde1de50afdc
Hagen Bracey <hagen.bracey@gmail.com>
2023-11-30 11:28:30 -0600

    Initial commit

---
+++ flashcards/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace flashcards
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainMenuPage();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 800;
+            const int newHeight = 600;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
+    }
+}
---
+++ flashcards/Flashcard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flashcards
+{
+    internal class Flashcard
+    {
+        private string? front = null;
+        private string? back = null;
+
+        public Flashcard()
+        {
+            front = back = "";
+        }
+
+        public Flashcard(string _front, string _back)
+        {
+            if (_front != null && _back != null)
+            {
+                front = _front;
+                back = _back;
+            }
+        }
+
+        public string Front
+        {
+            get
+            {
+                if (front != null) return front;
+                else return "";
+            }
+            set
+            {
+                front = value;
+            }
+        }
+
+        public string Back
+        {
+            get
+            {
+                if (back != null) return back;
+                else return "";
+            }
+
+            set
+            {
+                back = value;
+            }
+        }
+
+        public bool IsComplete
+        {
+            get
+            {
+                return (front != null && back != null);
+            }
+        }
+    }
+}
